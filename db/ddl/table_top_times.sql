6fc310eb2521752d023c2b0fbd6903ad5f499c20
Joe Krajcir <jkrajcir@yahoo.com>
2023-08-30 15:46:29 -0500

    changing game won date back to timestamp, so timestamp can be used to break ties

--- db/ddl/table_top_times.sql
+++ db/ddl/table_top_times.sql
@@ -8,11 +8,11 @@ CREATE TABLE IF NOT EXISTS game.top_times
     player_name character varying(20) COLLATE pg_catalog."default" NOT NULL,
     difficulty smallint NOT NULL,
     elapsed_time smallint NOT NULL,
-    game_won_date date NOT NULL,
+    game_won_timestamp timestamp with time zone NOT NULL,
     CONSTRAINT top_times_pkey PRIMARY KEY (id),
     CONSTRAINT top_times_difficulty_check CHECK (difficulty >= 0 AND difficulty <= 2),
     CONSTRAINT top_times_elapsed_time_check CHECK (elapsed_time > 0),
-    CONSTRAINT top_times_game_won_date_check CHECK (game_won_date <= CURRENT_DATE),
+    CONSTRAINT top_times_game_won_timestamp_check CHECK (game_won_timestamp < CURRENT_TIMESTAMP),
     CONSTRAINT top_times_player_name_check CHECK (length(player_name::text) > 0)
 )
 
