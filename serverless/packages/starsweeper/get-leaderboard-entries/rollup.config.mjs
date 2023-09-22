583c1d9a13d7a2fe3bad870dd8864cccb7b00755
Joe Krajcir <jrkrajcir@gmail.com>
2023-09-22 15:44:07 -0500

    get-leaderboard-entries: build scripts and config update

--- serverless/packages/starsweeper/get-leaderboard-entries/rollup.config.mjs
+++ serverless/packages/starsweeper/get-leaderboard-entries/rollup.config.mjs
@@ -1,11 +1,12 @@
 import alias from '@rollup/plugin-alias'
+import typescript from '@rollup/plugin-typescript'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import json from '@rollup/plugin-json'
 import commonjs from '@rollup/plugin-commonjs'
 import terser from '@rollup/plugin-terser'
 
 export default {
-  input: 'ts-out/get-leaderboard-entries.mjs',
+  input: 'src/get-leaderboard-entries.mts',
   output: {
     file: 'lib/get-leaderboard-entries.mjs',
     format: 'es'
@@ -13,9 +14,10 @@ export default {
   plugins: [
     alias({
       entries: {
-        '@common': '../../../../../common'
+        '@common': '../../../../lib/common'
       }
     }),
+    typescript(),
     nodeResolve({ exportConditions: ['node'] }),
     json(),
     commonjs(),
